99b80a84b3c5494d64a278c1ff5019533222b5ca
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-05-06 23:07:35 +0600

    data send to database

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
 import "./App.css";
 
 function App() {
+  const handleAddUser = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email };
+    console.log(user);
+
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
   return (
     <>
       <h1>Simple CRUD</h1>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" id="" placeholder="name" />
+        <br />
+        <input type="email" name="email" id="" placeholder="email" />
+        <br />
+        <input type="submit" value="Add user" />
+      </form>
     </>
   );
 }
