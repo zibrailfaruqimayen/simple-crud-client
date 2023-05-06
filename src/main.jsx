87294fb5a0b2c1524694458a5639c5e4d9e855e1
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-05-07 01:34:28 +0600

    n

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Users from "./components/Users.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App></App>,
+  },
+  {
+    path: "/users",
+    element: <Users></Users>,
+    loader: () => fetch("http://localhost:5000/users"),
   },
 ]);
 
