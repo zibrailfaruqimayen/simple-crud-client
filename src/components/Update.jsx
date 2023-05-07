d3626f02a3867909740e0257f37b1e6ca79447aa
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-05-07 16:06:41 +0600

    module end

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -8,8 +8,22 @@ const Update = () => {
     const form = event.target;
     const name = form.name.value;
     const email = form.email.value;
-
     console.log(name, email);
+    const updatedUser = { name, email };
+
+    fetch(`http://localhost:5000/users/${loadedUser._id}`, {
+      method: "PUT",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(updatedUser),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount > 0) {
+          alert("user updated successfully");
+          form.reset();
+        }
+      });
   };
 
   return (
